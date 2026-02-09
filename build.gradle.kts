445e8b94a0ff6aa168845152693ebae8c603f0b3
Buzz <oyvinder@outlook.com>
2026-02-09 14:50:26 +0100

    Test dependabot

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.10"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("com.typesafe.akka:akka-stream_3:2.8.6")
+    implementation("com.typesafe.akka:akka-actor_3:2.8.6")
+    implementation("org.apache.httpcomponents:httpclient:4.3.5")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.7.1)
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
